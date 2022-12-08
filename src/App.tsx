c57cf99fd76d03a5f4c9783635f9b2f9a1b109d8
Helder Burato Berto <helder.burato@gmail.com>
2022-12-08 10:00:28 +0000

    Cleanup project

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header>
+        <h1>Software Engineering Principles with ReactJS</h1>
+
+        <div>
+          <h2>SOLID</h2>
+        </div>
       </header>
     </div>
   );
